3c9438d0419c66240ec49ee2403d2a97cb8bf22a
FTintori <113966242+FTintori@users.noreply.github.com>
2023-02-26 20:54:57 -0700

    Update main.py

--- Github Python Projects/Weather Alerts/main.py
+++ Github Python Projects/Weather Alerts/main.py
@@ -2,16 +2,15 @@ import requests
 
 from twilio.rest import Client
 
-account_sid = 'ACf5f818f4ec14e8b34ebc7e9a48557f43'
-auth_token = '79e3691e6b388cc6a5c2e887ade4ddd0'
-APIkey = "69f04e4613056b159c2761a9d9e664d2"
-#"36d615cbd7cbda21fd3b478d2bab9f77"
-# "69f04e4613056b159c2761a9d9e664d2"
+account_sid = '*****'
+auth_token = '****'
+APIkey = "******"
+
 
 
 params = {
-            "lat": 51.080244,
-            "lon": -114.146742,
+            "lat": 51,
+            "lon": -114,
             "appid": APIkey,
             "exclude": "current,minutely,daily"}
 
@@ -20,8 +19,8 @@ def send_rain_sms():
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body='Today there is chance of rain, bring an umbrella!',
-        from_='+14793411959',
-        to='+15875774686'
+        from_='****',
+        to='*****'
         )
     print(message.status)
 
